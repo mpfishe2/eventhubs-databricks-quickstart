34408fe7d3b90f83b50abf047427c30fb1092152
mpfishe2 <mpfishe2@illinois.edu>
2018-08-17 12:36:03 -0500

    Update Real-Time-Demo-Python.py

--- Real-Time-Demo-Python.py
+++ Real-Time-Demo-Python.py
@@ -13,7 +13,7 @@ dftxn = Real_T_Stream.select(Real_T_Stream.body.cast("string").alias('message'))
 
 dftxn.writeStream \
   .outputMode("append") \
-  .format("delta") \
-  .option("path", "") \
-  .option("checkpointLocation", "") \
+  .format("json") \
+  .option("path", "") \ # storage of streamed files
+  .option("checkpointLocation", "") \ # checkpoint folder
   .start()
